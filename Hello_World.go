f39642bf81230e915ae9d41bdcd21dee416d22cc
larikhide <petrakliy@gmail.com>
2019-04-04 02:12:27 +0300

    test of commit 2

--- Hello_World.go
+++ Hello_World.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-// this is a comment
+// for make a string comment press ctrl + /
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World") /* for make a block comment press shift + alr + A */
 }
