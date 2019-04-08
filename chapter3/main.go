3bab845f9823b603bfb9ee211c66f9101d4dde7a
larikhide <petrakliy@gmail.com>
2019-04-08 23:49:12 +0300

    final version of chapter 3

--- chapter3/main.go
+++ chapter3/main.go
@@ -5,5 +5,6 @@ import "fmt"
 func main() {
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[1])
-	fmt.Println("Hello" + "World")
+	fmt.Println("Hello " + "World")
+	fmt.Println("32132 * 42452 = ", 32132*42452)
 }
