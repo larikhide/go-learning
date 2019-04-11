67aba47170c84095ec0dda102ddf7d3fe0bde2a0
ваше имя < >
2019-04-11 20:29:00 +0300

    start the outline of the chapter

--- chapter4/learn_the_variables.go
+++ chapter4/learn_the_variables.go
@@ -4,17 +4,17 @@ import "fmt"
 
 func main() {
 	var x string = "Hello World"
-	fmt.Println(x)
-	fmt.Print("Enter a foots: ") /* можно сделать так */
+	fmt.Println(x) /* можно сделать так */
 
 	var y string
-	x = "Hello World"
-	fmt.Println(x) /* а можно так */
+	y = "Hello World"
+	fmt.Println(y) /* а можно так */
+}
 
-	var foots float64
-	fmt.Scanf("%f", &foots)
-
-	meters := foots * 0.3048
-
-	fmt.Println("foots in metres are ", meters)
+func test() {
+	var x string
+	x = "first"
+	fmt.Println(x)
+	x = "second"
+	fmt.Println(x)
 }
