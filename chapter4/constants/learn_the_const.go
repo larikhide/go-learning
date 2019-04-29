e56560bf2313ad265d2c0ccb50899ed8ff290642
ваше имя < >
2019-04-29 17:04:51 +0300

    finish the lesson

--- chapter4/constants/learn_the_const.go
+++ chapter4/constants/learn_the_const.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	const x string = "Hello Wolrd"
+	const x string = "Hello Wolrd" /* значения констант не могут быть изменены после их иницаиализации. Константы - хороший способ использовать определенные значения в программе без необходимости писать их каждый раз */
 	fmt.Println(x)
 }
