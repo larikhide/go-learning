e85aa7d641508411f7116eec9ca2c62e76ea3df5
ваше имя < >
2019-04-29 16:57:30 +0300

    add const folder

--- chapter4/areaOfVisibility/area_of_visibility.go
+++ chapter4/areaOfVisibility/area_of_visibility.go
@@ -4,10 +4,8 @@ import "fmt"
 
 var x string = "Hello Tello" /* если задать переменную за функцией main, то к ней можно обращаться через другие функции */
 
-const g = "Tello Hotello" /* константу нельзя менять по ходу выполнении программы, иначе будет ошибка */
-
 func main() {
-	fmt.Println(g)
+	fmt.Println(x)
 }
 
 func f() {
