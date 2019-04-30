5179c057bed030414060e40f6e1094f4b0c79937
ваше имя < >
2019-04-30 17:00:11 +0300

    change location between %15 and %3

--- chapter5/fizzBizz/fizzBizz.go
+++ chapter5/fizzBizz/fizzBizz.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 	for number := 1; number <= 100; number++ {
-		switch number {
+		switch {
+		case number%15 == 0:
+			fmt.Println("FizzBizz")
 		case number%3 == 0:
 			fmt.Println("Fizz")
 		case number%5 == 0:
 			fmt.Println("Bizz")
-		case number%15 == 0:
-			fmt.Println("FizzBizz")
 		default:
 			fmt.Println(number)
 		}
