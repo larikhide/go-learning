b4e36b5a6ebe9b48d5a9f01f8104a7ca8c9a7c45
ваше имя < >
2019-04-30 16:33:26 +0300

    fixed fizzBizz

--- chapter5/fizzBizz/fizzBizz.go
+++ chapter5/fizzBizz/fizzBizz.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+	for number := 1; number <= 100; number++ {
+		switch number {
+		case number%3 == 0:
+			fmt.Println("Fizz")
+		case number%5 == 0:
+			fmt.Println("Bizz")
+		case number%15 == 0:
+			fmt.Println("FizzBizz")
+		default:
+			fmt.Println(number)
+		}
+	}
 
 }
