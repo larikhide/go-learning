e8ddf46e9b9495fc41759609e2fd37d2e2ba0989
ваше имя < >
2019-05-16 21:30:16 +0300

    created task3 programm

--- chapter7Functions/task2/task2.go
+++ chapter7Functions/task2/task2.go
@@ -9,9 +9,9 @@ func main() {
 	fmt.Println(maxElement(2, 4, 98, -43))
 }
 
-func maxElement(arg ...int) int {
+func maxElement(list ...int) int {
 	maxNum := math.MaxInt32
-	for _, value := range arg {
+	for _, value := range list {
 		if maxNum < value || maxNum == math.MaxInt32 {
 			maxNum = value
 		}
