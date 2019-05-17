ae88e7400b0e72ccb4f8975966e0e0dd96dfe3fb
ваше имя < >
2019-05-17 19:32:17 +0300

    fixed task1 of chapter9

--- chapter9StructsAndInterfaces/task1.go
+++ chapter9StructsAndInterfaces/task1.go
@@ -16,25 +16,27 @@ type Rectangle struct {
 	width, height float64
 }
 
-func (c *Circle) area() float64 {
+func (c Circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
-func (c *Circle) perimeter() float64 {
+func (c Circle) perimeter() float64 {
 	return 2 * math.Pi * c.radius
 }
-func (r *Rectangle) area() float64 {
+func (r Rectangle) area() float64 {
 	return r.width * r.height
 }
-func (r *Rectangle) perimeter() float64 {
+func (r Rectangle) perimeter() float64 {
 	return 2*r.width + 2*r.height
 }
-func measure (S Shape) {
+func measure(S Shape) {
 	fmt.Println(S)
-	fmt.Println(S.)
+	fmt.Println(S.area())
+	fmt.Println(S.perimeter())
 }
 func main() {
 	c := Circle{radius: 6}
 	r := Rectangle{width: 2, height: 4}
-	fmt.Println(c)
-	fmt.Println(r)
+	measure(c)
+	measure(r)
+
 }
