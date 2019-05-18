e890a6bf8ffb0a751186beeb50d5a8ff314bc562
larikhide <petrakliy@gmail.com>
2019-05-19 01:49:47 +0300

    very bad task of chapter10

--- chapter9StructsAndInterfaces/task1.go
+++ chapter9StructsAndInterfaces/task1.go
@@ -16,16 +16,16 @@ type Rectangle struct {
 	width, height float64
 }
 
-func (c Circle) area() float64 {
+func (c *Circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
-func (c Circle) perimeter() float64 {
+func (c *Circle) perimeter() float64 {
 	return 2 * math.Pi * c.radius
 }
-func (r Rectangle) area() float64 {
+func (r *Rectangle) area() float64 {
 	return r.width * r.height
 }
-func (r Rectangle) perimeter() float64 {
+func (r *Rectangle) perimeter() float64 {
 	return 2*r.width + 2*r.height
 }
 func measure(S Shape) {
