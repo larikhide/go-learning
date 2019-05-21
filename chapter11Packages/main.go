a6036139de59d206b052a16a58bed88a0fb589a8
ваше имя < >
2019-05-21 12:06:51 +0300

    finished chapter11

--- chapter11Packages/main.go
+++ chapter11Packages/main.go
@@ -4,7 +4,7 @@ import "fmt"
 import "D:/skripts/Study-to-be-a-golang-programmer/chapter11Packages/math"
 
 func main() {
-	xs := []float64{1, 2, 3, 4}
+	xs := []float64{}
 	avg := math.Average(xs)
 	fmt.Println(avg)
 }
