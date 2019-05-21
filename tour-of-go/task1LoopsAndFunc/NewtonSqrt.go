3ce8e4bce4641417432418d657fe6c8adcd11c34
ваше имя < >
2019-05-21 19:56:31 +0300

    1st step of task func and loops

--- tour-of-go/task1LoopsAndFunc/NewtonSqrt.go
+++ tour-of-go/task1LoopsAndFunc/NewtonSqrt.go
@@ -6,13 +6,13 @@ import (
 
 func Sqrt(x float64) float64 {
 	z := float64(1)
-	for i := 1; i <= 10; i++ {
-		z = z - (z*z-x)/2*z
-	}
+	for i := 0; i < 100000; i++ {
+		z = z - (z*z-x)/(2*z)
 
+	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(9))
+	fmt.Println(Sqrt(2))
 }
