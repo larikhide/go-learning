c237cf933aed065a115b32559dcbd4ab49550d18
ваше имя < >
2019-05-22 13:36:53 +0300

    fixed NewtonSqrt

--- tour-of-go/task1LoopsAndFunc/NewtonSqrt.go
+++ tour-of-go/task1LoopsAndFunc/NewtonSqrt.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
-	z := float64(1)
-	for i := 0; i < 100000; i++ {
+	z := 1.0
+	d := 0.0 // переменная для оценки точности
+	for i := 0; i < 10000; i++ {
 		z = z - (z*z-x)/(2*z)
-
+		if math.Abs(d-z) < 0.0001 && i != 0 { //оценка точности
+			break
+		}
+		d = z
 	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(0.1488))
 }
