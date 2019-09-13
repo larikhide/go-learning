a64b36e45c58538bfa0fd4a43388636e9640332c
ваше имя < >
2019-09-13 20:42:36 +0300

    draft of binarySearch func

--- grokking-algorithms/binarySearch/binarySearch.go
+++ grokking-algorithms/binarySearch/binarySearch.go
@@ -1,5 +1,25 @@
 package main
 
-func binarySearch([]int) int {
+func binarySearch(list []int, item int) pos int {
+	low:= 0
+	high:=len(list) - 1
+	for _, low <= high, i++ {
+		mid = (low+high)
+		guess = list[mid]
+		switch {
+		case guess == item:
+			return mid
+		case guess > item:
+			high = mid -1
+		default:
+			low = mid +1
+		}
+	}
+return pos
+}
 
+func main () {
+	list:= make([]int, 5)
+	fmt.Scanf(&list)
+	fmt.Println(binarySearch(list, 4))
 }
