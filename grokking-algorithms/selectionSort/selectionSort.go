d40f01ce48d420fe2f1d90ef3fa969a3211603fc
ваше имя < >
2019-09-24 19:00:45 +0300

    finished selectionSorting

--- grokking-algorithms/selectionSort/selectionSort.go
+++ grokking-algorithms/selectionSort/selectionSort.go
@@ -4,26 +4,57 @@ import (
 	"fmt"
 )
 
-// функция нахождения минимального числа в слайсе
-func findSmallest(list []int) (smallest int) {
-	smallest = list[0]
+/* // функция нахождения минимального числа в слайсе
+func findSmallest(list []int) int {
+	smallest := list[0]
+	//smallestID := list[0],
 	for _, min := range list {
 		if min < smallest {
 			smallest = min
+			//smallestID = i
 		}
 	}
 	return smallest
 }
 
-//функция сортировки выбором
-func selectionSort(
-//хуряить тут
-)
+//функция сортировки выбором после нахождения минимального
+func selectionSort(list []int) (sortedList []int) {
+	for _, el := range list {
+		el = findSmallest(list)
+		sortedList = append(sortedList, el)
+	}
+	return
+} */
+
+/* //функция сортировки выбором, всё в одной. не закончено
+func selectionSort(list []int) (sortedList []int) {
+	smallest := list[0]
+	for _, min := range list {
+		if min < smallest {
+			smallest = min
+		}
+		sortedList = append(sortedList, smallest)
+	}
+	return
+} */
+
+func selectionSort(list []int) []int {
+	for i := 0; i < len(list)-1; i++ {
+		id, min := i, list[i]
+		for j := i + 1; j < len(list); j++ {
+			if list[j] < min {
+				id, min = j, list[j]
+			}
+		}
+		list[id], list[i] = list[i], min
+	}
+	return list
+}
 
 func main() {
 	list := make([]int, 5)
 	for i := 0; i < 5; i++ {
 		fmt.Scan(&list[i])
 	}
-	fmt.Println(findSmallest(list))
+	fmt.Println(selectionSort(list))
 }
