a46104176909de709b26be41b0a1a8151d93b5dc
ваше имя < >
2019-10-30 18:24:05 +0300

    winished quickSort

--- grokking-algorithms/quickSort/quickSort.go
+++ grokking-algorithms/quickSort/quickSort.go
@@ -4,35 +4,72 @@ import (
 	"fmt"
 )
 
-func quickSort(arr []int32) []int32 {
-	var less, greater []int32
-	for i := range arr {
-		pivot := len(arr) / 2
-		switch {
-		/* case arr[i] == arr[pivot]:
-		return arr */
-		case arr[i] < arr[pivot]:
-			less[0] = arr[i]
-		case arr[i] >= arr[pivot]:
-			greater[0] = arr[i]
-		default:
-			return arr
-		}
+/* func quickSort(ar []int32) {
+	pivot := ar[len(ar)/2]
+	left := 0
+	right := len(ar) - 1
+	if len(ar) < 2 {
+		return
 	}
-	arr = append(less, greater...)
-	return arr
+	//проходимся по массиву
+	//получаем индекс элемента, относительного которого слева - всё, что меньше
+	//справа - больше
+	for { //выполняется пока не произойдет return
+		for ; ar[left] < pivot; left++ { //while ar[left] < pivot
+		}
+		for ; ar[right] > pivot; right-- { //same
+		}
+		if left >= right { //если условие выполнилось, то массив отсортирован на большее и меньшее относительно элемента под индексом right
+			break
+		}
+		if ar[left] == ar[right] { //проверка на равенство элемента слева и справа
+			break
+		}
+		ar[left], ar[right] = ar[right], ar[left]
+	}
+	quickSort(ar[:right])
+	quickSort(ar[right:])
+} */
+
+func quickSort(ar []int32) {
+	if len(ar) < 2 { //проверка на базовый случай
+		return
+	}
+	pivot := ar[len(ar)/2]
+	left := 0
+	right := len(ar) - 1
+
+Loop:
+	for {
+		switch {
+		case ar[left] < pivot:
+			left++
+		case ar[right] > pivot:
+			right--
+		case (left >= right):
+			break Loop
+		case (ar[left] == ar[right]):
+			left++
+		}
+		ar[left], ar[right] = ar[right], ar[left]
+	}
+	quickSort(ar[:right])
+	quickSort(ar[right:])
 }
 
 func main() {
-	var (
+	ar := []int32{1, 4, 0, 1, 0, 1, -14, 1}
+	//ввод с клавы
+	/* var (
 		n  int
 		el int32
 	)
 	fmt.Scan(&n)
-	arr := make([]int32, n)
-	for i := range arr {
+	ar := make([]int32, n)
+	for i := range ar {
 		fmt.Scan(&el)
-		arr[i] = el
-	}
-	fmt.Print(quickSort(arr))
+		ar[i] = el
+	} */
+	quickSort(ar)
+	fmt.Print(ar)
 }
