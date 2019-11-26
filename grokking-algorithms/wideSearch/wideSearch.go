14a02639f0bbae3e7ca5bf44868244ff76742511
ваше имя < >
2019-11-26 17:05:26 +0300

    added comments for mango trader

--- grokking-algorithms/wideSearch/wideSearch.go
+++ grokking-algorithms/wideSearch/wideSearch.go
@@ -28,20 +28,23 @@ func main() {
 		"Jonny": []string{},
 	}
 	visited := []string{}
-	bfs("you", persons, func(name string) {
-		visited = append(visited, name)
+	bfs("Bob", persons, func(name string) { //в функцию bfs передаем аргументы: имя, кого проверяем и его друзей; исходную хеш-таблицу;
+		visited = append(visited, name) // и функцию, которая к списку проверенных имен добавляет вновь проверенное имя
 	})
 	fmt.Println(visited)
 }
 
+/* функция поиска в ширину.
+Аргументы: имя, того кого проверяем; исходная структура по которой обходим и
+функция fn которая что-то делает с результатом поиска */
 func bfs(start string, persons map[string][]string, fn func(string)) {
-	frontier := []string{start}
-	visited := map[string]bool{}
-	next := []string{}
+	frontier := []string{start}  //изначально проверяемое имя
+	visited := map[string]bool{} //
+	next := []string{}           //
 
-	for 0 < len(frontier) {
-		next = []string{}
-		for _, name := range frontier {
+	for 0 < len(frontier) { //пока у проверяемой строки есть длина, то продолжать расчет
+		next = []string{}               //
+		for _, name := range frontier { //обход по значениям строки
 			visited[name] = true
 			fn(name)
 			for _, n := range bfsFrontier(name, persons, visited) {
@@ -52,11 +55,12 @@ func bfs(start string, persons map[string][]string, fn func(string)) {
 	}
 }
 
+/* функция берет имя, карту, есть ли значение под ключом, выдает массив строк */
 func bfsFrontier(name string, persons map[string][]string, visited map[string]bool) []string {
-	next := []string{}
-	iter := func(n string) bool { _, ok := visited[string(n)]; return !ok }
-	for _, n := range persons[name] {
-		if iter(n) {
+	next := []string{}                                                      //объявляем массив строк
+	iter := func(n string) bool { _, ok := visited[string(n)]; return !ok } //объявляем функцию как значение. Получаем значение из карты по имени. Возвращаем хуй знает что и зачем
+	for _, n := range persons[name] {                                       //обходим исходную мапу по значениям
+		if iter(n) { //если
 			next = append(next, string(n))
 		}
 	}
