5db252558b66f8664c76f488b6b5e8b9cc8641be
larikhide <petrakliy@gmail.com>
2020-02-19 12:21:50 +0300

    finished 2nd task

--- homework-3/hw3.go
+++ homework-3/hw3.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-//Machine is a structire
+//Machine 1. Описать несколько структур
 type Machine struct {
 	Label   string
 	Year    uint
@@ -13,13 +13,30 @@ type Machine struct {
 }
 
 func main() {
+	//2. инициализировать несколько структур
 	var lightCar = Machine{
-		Label:   'lada',
+		Label:   "lada",
 		Year:    2007,
 		Volume:  40.2,
 		Engine:  true,
 		Windows: true,
 		Fill:    0.5,
 	}
-	fmt.Println(lightCar)
+
+	var heavyCar = Machine{
+		Label:   "Scania",
+		Year:    2014,
+		Engine:  false,
+		Windows: true,
+		Fill:    1,
+	}
+
+	//2. Применить к ним различные действия.
+	lightCar.Year++
+	heavyCar.Windows = false
+	heavyCar.Volume = lightCar.Volume + 1050
+
+	//2. Вывести значения свойств экземпляров в консоль.
+	fmt.Println(lightCar, heavyCar)
+	fmt.Println(heavyCar.Label, lightCar.Year)
 }
