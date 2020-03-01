5c937b83d506b0e49b47fc1c0bbadf95b9c95af9
larikhide <petrakliy@gmail.com>
2020-03-01 21:04:06 +0300

    finished task3 with nor workig reader

--- homework-5/fileread.go
+++ homework-5/fileread.go
@@ -3,27 +3,56 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
+	"io"
 )
+/*
+2. Что бы вы изменили в программе чтения из файла? Приведите исправленный вариант, обоснуйте свое решение в комментарии. */
 
+// файл может быть большого размера и лучше читать его построчно или по определенному количеству байт
+
+//чтение побайтово
 func main() {
+
+	//открыли файл
 	file, err := os.Open("fileread.go")
 	if err != nil {
+		fmt.Println("Unable to open file:", err)
 		return
 	}
 	defer file.Close()
 
-	// getting size of file
-	stat, err := file.Stat()
-	if err != nil {
-		return
-	}
 
-	//reading file
-	bs := make([]byte, stat.Size())
-	_, err = file.Read(bs)
-	if err != nil {
-		return
-	}
+	//прочли по 64 байта
+	bs := make([]byte, 64)
+	for{
+        n, err := file.Read(bs)
+        if err == io.EOF{   // если конец файла
+            break           // выходим из цикла
+        }
+        fmt.Print(string(bs[:n]))
+    }
 
-	fmt.Println(string(bs))
+
+//построчное чтение из файла
+	anFile, err := os.Open("task1.txt")
+    if err != nil {
+         fmt.Println("Unable to open file:", err)
+         return
+    }
+    defer anFile.Close()
+
+    reader := bufio.NewReader(anFile)
+    for {
+        line, err := reader.ReadString('\n')
+        if err != nil {
+            if err == io.EOF {
+                break
+            } else {
+                fmt.Println(err)
+                return
+            }
+        }
+        fmt.Print(line)
+    }
 }
--- homework-5/filereadshort.go
+++ homework-5/filereadshort.go
@@ -1,15 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
 func main() {
 
 	// open whole file, read and close after
-	/* 	sh, err := ioutil.ReadFile("filereadshort.go")
-	   	if err != nil {
-	   		return
-	   	}
-	   	fmt.Println(string(sh)) */
+	sh, err := ioutil.ReadFile("filereadshort.go")
+	if err != nil {
+		return
+	}
+	fmt.Println(string(sh))
 
-	/* //read directory
+	//read directory
 	dir, err := os.Open(".")
 	if err != nil {
 		return
@@ -22,22 +29,22 @@ func main() {
 	}
 	for _, fi := range fileInfos {
 		fmt.Println(fi.Name())
-	} */
+	}
 
 	//create a file and write something into
-	/* file, err := os.Create("task1.txt")
+	file, err := os.Create("task1.txt")
 	if err != nil {
 		return
 	}
 
 	defer file.Close()
 
-	file.WriteString("тут я напишу что-нибудь по первому заданию ДЗ номер 5") */
+	file.WriteString("тут я напишу что-нибудь по первому заданию ДЗ номер 5")
 
 	//Рекурсивный обход каталогов
-	/* filepath.Walk("../..", func(path string, info os.FileInfo, err error) error {
+	filepath.Walk("../..", func(path string, info os.FileInfo, err error) error {
 		fmt.Println(path)
 		return nil
-	}) */
+	})
 
 }
