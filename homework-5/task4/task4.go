be9b4d5b1b16105b22bc514b6b8b739457a33c60
ваше имя < >
2020-03-02 12:14:17 +0300

    finished task4 from hw-5

--- homework-5/task4/task4.go
+++ homework-5/task4/task4.go
@@ -2,13 +2,44 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
+var pName = os.Args[0]
+var original = flag.String("from", "", "Name of original")
+var copy = flag.String("copy", "", "Name of copy")
+
+//help for user
+func help() {
+	fmt.Println("чтобы скопировать файл введите в консоль")
+	fmt.Printf("%s -original=original-name.txt -copy=copy-name.txt\n", pName)
+	flag.PrintDefaults()
+}
+
 func main() {
-	pName := os.Args[0]
-	original := flag.String("from", "default value", "Name of original")
-	copy := flag.String("copy", "default", "Name of copy")
 	flag.Parse()
+	//если один из параметров не указан, то вывыедет справку
+	if *original == "" || *copy == "" {
+		help()
+	}
+
+	// open whole file, read and close after
+	//прочитать весь файл будет лучше, т.к. мы хотим скопировать весь файл целиком
+	//если же будем читать построчно или побайтово, то мало ли что-то недокопируется
+	// не знаю такое может быть или нет,но пока это вижу так
+	fData, err := ioutil.ReadFile(*original)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nFile, err := os.Create(*copy)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nFile.Close()
+	nFile.WriteString(string(fData))
 
 }
