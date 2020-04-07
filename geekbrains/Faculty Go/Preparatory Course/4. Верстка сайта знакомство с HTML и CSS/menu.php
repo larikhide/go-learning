59f78d3256f9253ea42c653aa798ca8e8ef72296
larikhide <petrakliy@gmail.com>
2020-04-07 23:34:53 +0300

    deleted html css materials

--- geekbrains/Faculty Go/Preparatory Course/4. Верстка сайта знакомство с HTML и CSS/menu.php
+++
@@ -1,7 +0,0 @@
-<div class="header">
-        <a href="#">Главная</a>
-        <a href="puzzle.html">Загадки</a>
-        <a href="guess.html">Угадайка</a>
-        <a href="guess-2.html">Угадайка PVP</a>
-        <a href="password_gen.html">Генератор паролей</a>
-    </div>
